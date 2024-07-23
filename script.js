b2d64cece8e519704dc3c702f6a97d2ce7cd2170
leticiaSouzaDev <lec667121@gmail.com>
2024-07-23 16:04:29 -0300

    feat: finalização dos exercicios

--- script.js
+++ script.js
@@ -112,4 +112,103 @@
 
 // console.log(substituicao)
 
+// #7-criar um loop while em Python que itera sobre os itens de uma lista e imprime os itens enquanto o valor 
+// de uma variável é menor ou igual a 5. Após imprimir cada item, o valor da variável é incrementado em 1
+// #explique detalhadamente por que escolheu essa solução e não outra
 
+// const lista = [1, 2, 3, 4, 5, 6];
+// let valor = 0; // Inicializamos 'valor' em 0
+
+// // Loop 'while' que continuará enquanto 'valor' for menor ou igual a 5
+// while (valor <= 5) {
+//     console.log(lista[valor]); // Imprime o item atual da lista usando o índice 'valor'
+    
+//     valor++; // Incrementa o valor após imprimir o item
+// }
+
+//#8-usando a biblioteca requests, faça uma requisição http para o endpoint https://jsonplaceholder.typicode.com/todos. 
+//cada objeto dentro do json possui a chave "completed". que indica se a task foi completada ou não. 
+//Faça uma função que trate a resposta e retorne a quantidade de tasks completadas, e a quantidade de 
+//tasks pendentes
+//#explique detalhadamente por que escolheu essa solução e não outra
+
+// async function requicicao(){
+//     const url = await fetch('https://jsonplaceholder.typicode.com/todos')
+//     const urlConvertida = await url.json();
+
+//     let valorTaskCompletas = 0;
+//     let valorTaskIncompletas = 0;
+
+//     urlConvertida.forEach(task => {
+//         if(task.completed){
+//             valorTaskCompletas++;
+//         } else{
+//             valorTaskIncompletas++;
+//         }
+//     })
+
+//     console.log(`Completas: ${valorTaskCompletas} Incompletas: ${valorTaskIncompletas}`)
+// }
+
+// requicicao()
+
+
+// #9-faça uma requisição em uma API  https://jsonplaceholder.typicode.com/users e com os dados retornados 
+// # faça um parsing de dados e retire  o nome, username, email, rua, numero
+// #explique detalhadamente por que escolheu essa solução e não outra
+
+// async function requisicaoApi(){
+//     const url = await fetch('https://jsonplaceholder.typicode.com/users');;
+//     const urlConvertida =  await url.json();
+
+//     const nomes = [];
+//     const usernames = [];
+//     const emails = [];
+//     const ruas = [];
+//     const numeros= [];
+
+//     urlConvertida.forEach(user => {
+//         nomes.push(user.name);                  
+//         usernames.push(user.username);          
+//         emails.push(user.email);                
+//         ruas.push(user.address.street);         
+//         numeros.push(user.address.suite);       
+
+       
+//         console.log(`
+//             Nome: ${user.name}
+//             Username: ${user.username}
+//             Email: ${user.email}
+//             Rua: ${user.address.street}
+//             Número: ${user.address.suite}
+//         `);
+//     });
+// }
+
+// requisicaoApi();
+
+
+// #10-crie uma lista e adicione um item novo a ela utilizando a metodologia FIFO
+
+// Criação de uma lista usando a metodologia FIFO
+// const fila = [];
+
+// fila.push('Primeiro');
+// fila.push('Segundo');
+// fila.push('Terceiro');
+
+// Removendo o primeiro elemento da lista (início da lista)
+// const elementoRemovido = fila.shift();
+
+// #11-crie uma lista e adicione um item novo a ela utilizando a metodolofia LIFO
+
+// Criação de uma lista usando a metodologia LIFO
+// const pilha = [];
+
+// Adicionando elementos à lista (fim da lista)
+// pilha.push('Primeiro');
+// pilha.push('Segundo');
+// pilha.push('Terceiro');
+
+// Removendo o último elemento da lista (fim da lista)
+// const elementoRemovido = pilha.pop();
